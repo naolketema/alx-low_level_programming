21aada65d0537ad7d152965b347ea46f25a9c30c
Naol Ketema <naolketema55@gmail.com>
2023-07-24 23:31:06 +0300

    simple task but betty ohh

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+
+	return (l);
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: type of character
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j = j + 2;
+	}
+
+	_putchar('\n');
+}
